f57d87dafc1127f4eb6677d309103db234dbeaa8
Alexander Bogarsukov <a.bogarsukov@gmail.com>
2018-09-03 22:15:46 +0300

    Added the code

---
+++ src/Build.ps1
@@ -0,0 +1,58 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+$solutionPath = Join-Path $scriptPath "UnityFx.DependencyInjection.sln"
+$configuration = $args[0]
+$packagesPath = Join-Path $scriptPath "..\temp\BuildTools"
+$binPath = Join-Path $scriptPath "..\bin"
+$msbuildPath = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MsBuild.exe"
+$nugetPath = Join-Path $packagesPath "nuget.exe"
+$gitversionPath = Join-Path $packagesPath "gitversion.commandline\tools\gitversion.exe"
+
+Write-Host "BasePath:" $scriptPath
+Write-Host "PackagePath:" $packagesPath
+Write-Host "BinPath:" $binPath
+
+# create output folders if needed
+if (!(Test-Path $packagesPath))
+{
+	New-Item $packagesPath -ItemType Directory
+}
+
+if (!(Test-Path $binPath))
+{
+	New-Item $binPath -ItemType Directory
+}
+
+# download nuget.exe if not present
+if (!(Test-Path $nugetPath))
+{
+	Write-Host "Install NuGet" -Foreground Blue
+	Invoke-WebRequest "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -Outfile $nugetPath
+}
+
+# install & run GitVersion
+Write-Host "Install/update GetVersion" -Foreground Blue
+& $nugetPath install -excludeversion gitversion.commandline -outputdirectory $packagesPath
+& $gitversionPath /l console /output buildserver
+
+# build projects
+Write-Host "Building projects" -Foreground Blue
+& $msbuildPath $solutionPath /m /v:m /t:Restore
+& $msbuildPath $solutionPath /m /t:Build /p:Configuration=$configuration
+& $msbuildPath $solutionPath /m /v:m /t:Pack /p:Configuration=$configuration
+
+# fail if solution build failed
+if ($LastExitCode -ne 0)
+{
+	if ($env:CI -eq $True)
+	{
+		$host.SetShouldExit($LastExitCode)
+	}
+	else
+	{
+		exit($LastExitCode);
+	}
+}
+
+# publish build results to .\bin
+$filesToPublish = (Join-Path $scriptPath (Join-Path "UnityFx.DependencyInjection\bin" (Join-Path $configuration "\*")))
+Copy-Item -Path $filesToPublish -Destination $binPath -Force -Recurse
